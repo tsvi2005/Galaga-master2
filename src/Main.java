19cb0f8d4bafd072ec3b18061f843446f8121b5a
tsvi2005 <tsvi2005@gmail.com>
2022-04-27 13:32:38 +0300

    update

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main () {
+        GalagaPanel galagaPanel = new GalagaPanel(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        this.add(galagaPanel);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        this.setVisible(true);
+
+
+
+    }
+
+
+
+}
